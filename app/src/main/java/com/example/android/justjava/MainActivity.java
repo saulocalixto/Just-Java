7d8ad05b80e5c812e9129dca2c343da49585ac5f
Saulo de Araújo Calixto <sauloacalixto@gmail.com>
2016-12-25 12:39:15 -0200

    Projeto Android Just Java

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.android.justjava;
+
+/**
+ * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava;
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    int numberOfCoffes = 0;
+    double preco = 5.5;
+
+    public void increment(View view) {
+
+        numberOfCoffes++;
+        display(numberOfCoffes);
+    }
+
+    public void decrement(View view) {
+
+        if(numberOfCoffes != 0) {
+            numberOfCoffes--;
+            display(numberOfCoffes);
+        }
+    }
+
+
+    public void submitOrder(View view) {
+        String priceMessage = "Total: R$" + (numberOfCoffes * preco) + "\nObrigado pela compra.";
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+}
